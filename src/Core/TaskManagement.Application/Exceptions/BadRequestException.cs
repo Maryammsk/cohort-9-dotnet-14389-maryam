afef56d6a17f89b94a7d1b4b160fe72ca216bb8e
01-135231-048 <01-135231-048@student.bahria.edu.pk>
2026-08-11 11:59:46 +0500

    feat: add Serilog structured logging and global exception handling middleware

---
+++ src/Core/TaskManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message)
+        : base(message)
+    {
+    }
+}
